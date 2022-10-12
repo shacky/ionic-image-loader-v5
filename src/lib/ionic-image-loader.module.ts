6db156efa5009807361d34ba986e0e7505eb6514
shacky <shacky7@gmail.com>
2022-10-12 14:31:26 +0200

    Update ionic-image-loader.module.ts

--- src/lib/ionic-image-loader.module.ts
+++ src/lib/ionic-image-loader.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {IonicModule} from '@ionic/angular';
-import {File} from '@ionic-native/file/ngx';
+import {File} from '@awesome-cordova-plugins/file/ngx';
 
 import {IonicImageLoaderComponent} from './ionic-image-loader.component';
 import {ImageLoaderConfigService} from './services/image-loader-config.service';
